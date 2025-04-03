4fca0c5d50623f6940138745fc083140d1703b61
strobecat <203321455+strobecat@users.noreply.github.com>
2025-04-03 13:46:19 +0000

    bump(main/netmuxd): 0.2.1-p20250216

--- packages/netmuxd/build.sh
+++ packages/netmuxd/build.sh
@@ -2,16 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://github.com/jkcoxson/netmuxd
 TERMUX_PKG_DESCRIPTION="A replacement/addition to usbmuxd which is a reimplementation of Apple's usbmuxd on MacOS"
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@xunmod"
-_COMMIT=4790aff72448929ce1074cdc07c8db79676c2fb6
-_COMMIT_DATE=20241114
-TERMUX_PKG_VERSION=0.1.4-p${_COMMIT_DATE}
-TERMUX_PKG_REVISION=1
+_COMMIT=6b4941dbed8fac38c67db031be0309717ff6b4e3
+_COMMIT_DATE=20250216
+TERMUX_PKG_VERSION=0.2.1-p${_COMMIT_DATE}
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_SRCURL=git+https://github.com/jkcoxson/netmuxd
-TERMUX_PKG_SHA256=146918eb1580458199c9b29e4a3ed31a9e56d020303b4ff4302df18132826410
+TERMUX_PKG_SHA256=b5a8a9d0de8d70abc9a5f4c56a150c5aa6f1a51e9299cfa62857bf2e62ed1d4f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="libimobiledevice, libplist, openssl"
+TERMUX_PKG_DEPENDS="libplist, openssl"
 
 termux_step_post_get_source() {
 	git fetch --unshallow
